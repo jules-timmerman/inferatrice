5ee79ec30090cd644994746af468f81a55bd61b4
LostExcalibur <romaindebeaucorps@yahoo.fr>
2022-11-29 13:50:43 +0100

    Review unify

--- unify.ml
+++ unify.ml
@@ -3,16 +3,12 @@ open Term
 exception Unification_failure
 
 
-
 (*Return true si v est une variable de t*)
 let rec look_for (v : var) (t : t) : bool =
   match t with
   | Var(x) -> var_equals x v
   | Fun (s, []) -> false
-  | Fun (s, hd::tl) when look_for v hd -> true
-  | Fun (s, hd::tl)  -> look_for v (Fun (s, tl))
-
-
+  | Fun (s, hd::tl) -> look_for v hd || look_for v (Fun (s, tl))
 
 
 (** La fonction unify prend deux termes et effectue des instantiations
@@ -24,26 +20,20 @@ let rec look_for (v : var) (t : t) : bool =
   * en cas d'échec. *)
 let rec unify (t1: t) (t2: t) : unit =
   if equals t1 t2 then
-    () (*déja unifiés*)
+    () (* déja unifiés *)
   else
     match observe t1, observe t2 with
-    | Var(x),t -> (*Cas une variable et un terme : on unifie si la variable n'est pas dans t *)
+    | Var(x),t -> (* Cas une variable et un terme : on unifie si la variable n'est pas dans t *)
       if look_for x t then 
         raise Unification_failure
       else
         bind x t
-    | t, Var y -> (*Cas symétrique*)
+    | t, Var y -> (* Cas symétrique *)
       if look_for y t then 
         raise Unification_failure
       else
         bind y t
-    | Fun (s1, l1), Fun (s2, l2) when s1<>s2 -> raise Unification_failure (*Si deux fct différentes : impossible d'unifier*)
-    | Fun (s1, []), Fun (s2, l2) -> raise Unification_failure (*pas le meme nb de paramètres : liste vide*)
-    | Fun (s1, l1), Fun (s2, []) -> raise Unification_failure (*idem*)
-    | Fun (s1, hd1::tl1), Fun (s2, hd2::tl2) -> unify hd1 hd2 ; unify (Fun(s1,tl1)) (Fun(s2,tl2)) (*On unifie terme par terme*)
-
-
-
-
-
-
+    | Fun (s1, _), Fun (s2, _) when s1<>s2 -> raise Unification_failure (* Si deux fct différentes : impossible d'unifier *)
+    | Fun (_, []), Fun (_, _) -> raise Unification_failure (* pas le meme nb de paramètres : liste vide *)
+    | Fun (_, _), Fun (_, []) -> raise Unification_failure (* idem *)
+    | Fun (s1, hd1::tl1), Fun (s2, hd2::tl2) -> unify hd1 hd2 ; unify (Fun(s1,tl1)) (Fun(s2,tl2)) (* On unifie terme par terme *)

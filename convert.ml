890f919afa2825e6db4aa55e656538ccbe34602a
snoopinou30 <jules.snoopi@live.fr>
2022-11-30 13:49:33 +0100

    Convert avec Equals pour unify les arguments Log des requêtes traitées

--- convert.ml
+++ convert.ml
@@ -1,8 +1,7 @@
 
-(** Fonction de conversion avec empactage *)
+(** Fonction de conversion d'un Ast.Atom en Query avec empactage *)
 let convert (a : string Ast.Atom.t) : Query.t = 
   let n,l= (Ast.Atom.convert (Term.convert_var) a) in Query.Atom(n,l)
-
 
 (** Prends des premices et les AND ensemble pour donner une Query*)
 let rec build_and_query (liste: string Ast.Atom.t list) : Query.t = 
@@ -11,6 +10,36 @@ let rec build_and_query (liste: string Ast.Atom.t list) : Query.t =
   | [a] -> convert a
   | t::q -> Query.And(convert t, build_and_query q)
 
+
+(** Filter en appliquant unify en terme à terme entre les arguments de la conclusion et les arguments du paramètres*)
+(* let filter_unify (terms : Term.t list) ((a,_) : string Ast.Rule.t) : bool = 
+  let _,l = Ast.Atom.convert (Term.convert_var) a in
+  try (List.iter2  Unify.unify l terms) ; true with
+    |Unify.Unification_failure -> false *)
+
+
+(** Renvoie un AND avec des tests d'égalités entre les paramètres de l'atome et de la liste de termes *)
+let and_query_args (terms: Term.t list) (a: string Ast.Atom.t) : Query.t =
+  let _,l = Ast.Atom.convert (Term.convert_var) a in
+  assert (List.length l = List.length terms) ; (* On vérifie si on a la même taille de listes *)
+  let rec aux l1 l2 = match l1,l2 with
+    |[],[] -> Query.True
+    |[a],[b] -> Equals(a,b)
+    |t1::q1, t2::q2 -> And(Equals(t1,t2), aux q1 q2)
+    |_ -> failwith("Not the same length")
+  in aux terms l
+
+
+(** Prends des règles et le terme original 
+    Renvoie une query représentant un OU des différentes query composées de AND entre les arguments et les prémices *)
+let rec or_equals_query (terms: Term.t list) (rules : string Ast.Rule.t list) : Query.t =
+  match rules with
+    |[] -> Query.False
+    |[(a,premices)] -> And(and_query_args terms a, build_and_query premices)
+    |(a,premices)::q -> Or(And(and_query_args terms a, build_and_query premices), or_equals_query terms q)
+
+
+(* TODO : refaire sans fold pour éviter le False inutile*)
 (** Conversion des règles parsées en un [atom_to_query_t] utilisable
   par [Query.search] pour résoudre des requêtes en intégrant les
   règles d'inférence données. *)
@@ -20,14 +49,21 @@ let rules (rules: (string Ast.Atom.t * string Ast.Atom.t list) list) : Query.ato
     let terms_length = List.length terms in
     let filtered = List.filter (fun (Ast.Atom.Atom(s,l),_) -> s = name && List.length l = terms_length) rules in
     
+    (* Problème : on ne limite jamais avec un unify les règles
+       cf plus(0,n,n) va s'unifier avec plus(1,2,3) *)
+    (* Solution ? : On va essayer d'unifier termes à termes les arguments des règles *)
+    (* On va plutôt utiliser l'arnaque suivante : 
+       On va metre des tests d'égalités entre les paramètres dans dans le AND de la requête
+       Ca permet de forcer, LORS DE L'EVALUATION, l'unification.
+       On pourrait le faire maintenant mais ca demanderait d'annuler les modifications de l'unify puis de le refaire...*)
+
     (* On OR les premices des règles satisfantes ensembles en les convertissant en Query*)
-    List.fold_left 
-      (fun (x: Query.t) ((_,y): string Ast.Atom.t * string Ast.Atom.t list) 
-        -> Query.Or(x, build_and_query y)) 
-      Query.False filtered
+    or_equals_query terms filtered
 
 (** Conversion d'une liste d'atomes parsés en une requête conjonctive.
     La fonction renvoyée peut être appelée quand une solution aura été
     trouvée: elle affiche l'état des variables à ce moment là. *)
 let query (atomes: string Ast.Atom.t list) : Query.t * (unit -> unit) =
-  build_and_query atomes, fun () -> Format.printf "%a" Term.pp_state None
+  build_and_query atomes, 
+  (* fun () -> Format.printf "%a" Term.pp_state None *)
+  fun () -> Term.pp_state (Format.std_formatter) None
--- query.ml
+++ query.ml
@@ -26,38 +26,38 @@ let default_has: atom_to_query_t = fun s l ->
   False
 
 
-let rec has_solution ?(atom_to_query = default_has)  (t:t) : bool = match t with
-  | True -> true
-  | False -> false
-  | And(t1,t2) ->  
-    (* On devra calculer les deux donc on fait aussi les modifications associées pour avoir la réponse*)
-    let b1 = has_solution ~atom_to_query:atom_to_query t1 in
-    b1 && (has_solution ~atom_to_query:atom_to_query t2)
-  | Or(t1,t2) -> 
-    (* On calcule jusqu'à avoir un résultat, au quel cas on change l'environnement, sinon on restore*)
-    let save = Term.save () in
-    if (has_solution ~atom_to_query:atom_to_query t1) then
-      true  
-    else (
-      Term.restore save ; let save = Term.save () in
-      if (has_solution ~atom_to_query:atom_to_query t2) then
-        true
-      else(
-        Term.restore save ; false
+let rec has_solution ?(atom_to_query = default_has)  (t:t) : bool = 
+  Format.printf "\n\nTraitement de la query : " ; pp Format.std_formatter t ;
+  match t with
+    | True -> true
+    | False -> false
+    | And(t1,t2) ->  
+      (* On devra calculer les deux donc on fait aussi les modifications associées pour avoir la réponse*)
+      let b1 = has_solution ~atom_to_query:atom_to_query t1 in
+      b1 && (has_solution ~atom_to_query:atom_to_query t2)
+    | Or(t1,t2) -> 
+      (* On calcule jusqu'à avoir un résultat, au quel cas on change l'environnement, sinon on restore*)
+      let save = Term.save () in
+      if (has_solution ~atom_to_query:atom_to_query t1) then
+        true  
+      else (
+        Term.restore save ; let save = Term.save () in
+        if (has_solution ~atom_to_query:atom_to_query t2) then
+          true
+        else(
+          Term.restore save ; false
+        )
       )
-    )
-  | Equals(t1, t2) -> 
-  begin 
-    try (Unify.unify t1 t2 ; true) with
-      |Unify.Unification_failure -> false
-  end
-  | Atom(n,l) -> let q = atom_to_query n l in has_solution ~atom_to_query:atom_to_query q
-
-and has_solution_no_modification (atom_to_query : atom_to_query_t) (t:t) : bool =
-  let s = Term.save () in 
-  let b = has_solution t ~atom_to_query:atom_to_query in
-  Term.restore s ; b 
+    | Equals(t1, t2) -> 
+    begin 
+      try (Unify.unify t1 t2 ; true) with
+        |Unify.Unification_failure -> false
+    end
+    | Atom(n,l) -> let q = atom_to_query n l in has_solution ~atom_to_query:atom_to_query q
 
 let search ?(atom_to_query = default_search) (cont:(unit -> 'a)) (t:t) : unit =
   if (has_solution ~atom_to_query:atom_to_query t) then
     ignore (cont ())
+  else(
+    Format.printf "Pas de solution...\n"
+  )

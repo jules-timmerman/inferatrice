4e68fe8118a0cd05977210d2ce493f7aabd369a9
snoopinou30 <jules.snoopi@live.fr>
2022-11-30 17:58:21 +0100

    Clean Query

--- query.ml
+++ query.ml
@@ -26,36 +26,9 @@ let default_has: atom_to_query_t = fun s l ->
   False
 
 
-(* let rec has_solution ?(atom_to_query = default_has)  (t:t) : bool = 
-  Format.fprintf Format.std_formatter "\n\nTraitement de la query : \n%a\n" pp t ;
-  match t with
-    | True -> true
-    | False -> false
-    | And(t1,t2) ->  
-      (* On devra calculer les deux donc on fait aussi les modifications associées pour avoir la réponse*)
-      let b1 = has_solution ~atom_to_query:atom_to_query t1 in
-      b1 && (has_solution ~atom_to_query:atom_to_query t2)
-    | Or(t1,t2) -> 
-      (* On calcule jusqu'à avoir un résultat, au quel cas on change l'environnement, sinon on restore*)
-      let save = Term.save () in
-      if (has_solution ~atom_to_query:atom_to_query t1) then
-        true  
-      else (
-        Term.restore save ; let save = Term.save () in
-        if (has_solution ~atom_to_query:atom_to_query t2) then
-          true
-        else(
-          Term.restore save ; false
-        )
-      )
-    | Equals(t1, t2) -> 
-    begin 
-      try (Unify.unify t1 t2 ; true) with
-        |Unify.Unification_failure -> false
-    end
-    | Atom(n,l) -> let q = atom_to_query n l in has_solution ~atom_to_query:atom_to_query q *)
-
 exception End
+exception Found
+
 
 let rec search ?(atom_to_query = default_search) (cont:(unit -> 'a)) (t:t) : unit =
   Format.fprintf Format.std_formatter "\n\nTraitement de la query : \n%a\n" pp t ;
@@ -84,15 +57,7 @@ let rec search ?(atom_to_query = default_search) (cont:(unit -> 'a)) (t:t) : uni
 
 
 
-exception Found
 let has_solution ?(atom_to_query = default_has)  (t:t) : bool =
   try search ~atom_to_query:atom_to_query (fun () -> raise Found) t ; false
     with 
     | Found -> true
-
-(* let search ?(atom_to_query = default_search) (cont:(unit -> 'a)) (t:t) : unit =
-  if (has_solution ~atom_to_query:atom_to_query t) then
-    ignore (cont ())
-  else(
-    Format.printf "Pas de solution...\n"
-  ) *)
